22dfbd7f47a0ae94204ef7af43e2cc399cfb3401
Anubhav Paras <anubhavparas94@gmail.com>
2021-09-30 03:30:31 -0400

    added a dummy test to test the coverage of the template code.

--- test/test.cpp
+++ test/test.cpp
@@ -1,7 +1,9 @@
 /** Copyright 2021 Anubhav Paras  */
 
 #include <gtest/gtest.h>
+#include <lib.hpp>
 
-TEST(dummy, should_pass) {
+TEST(dummy_test, should_pass) {
+  dummy();
   EXPECT_EQ(1, 1);
 }
